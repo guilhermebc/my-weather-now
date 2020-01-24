656f5ddf48cc930a29ac8f540a097bc6170225c1
Guilherme Bezerra da Costa <guilherme.costa@totvs.com.br>
2020-01-24 01:27:08 -0300

    weather layout pt1

--- projects/ngx-weather/src/app/app.module.ts
+++ projects/ngx-weather/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './components/card/card.component';
+import { DegreeValueComponent } from './components/degree-value/degree-value.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    DegreeValueComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
