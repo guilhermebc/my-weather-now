142099bd3626aad806f5aa543a8a989aa3e14c60
Guilherme Costa <guilhermebc@live.com>
2020-01-27 01:41:48 -0300

    pipe temp converter refactor

--- projects/ngx-weather/src/app/pipes/temperature-converter.pipe.ts
+++ projects/ngx-weather/src/app/pipes/temperature-converter.pipe.ts
@@ -4,15 +4,16 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'temperatureConverter'
 })
 export class TemperatureConverterPipe implements PipeTransform {
+  //  input as Kelvin
   transform(value: number, unit: string) {
     if (value && !isNaN(value)) {
       if (unit === 'C') {
-        const temperature = (value - 32) / 1.8;
-        return temperature.toFixed(2);
+        const temperature = value - 273.15;
+        return temperature.toFixed(0);
       }
       if (unit === 'F') {
-        const temperature = value * 32 + 1.8;
-        return temperature.toFixed(2);
+        const temperature = ((value - 273.15) * 1.8) + 32;
+        return temperature.toFixed(0);
       }
     }
     return;
